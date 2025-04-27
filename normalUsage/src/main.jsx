0a86ac81790b3d10f493f5a33fd8c3a4b1bb8399
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-27 14:32:56 +0200

    feat: Main application and empty / path as a Router

---
+++ normalUsage/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import routes from "./routes";
+
+createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={routes} />
+    </React.StrictMode>
+);
---
+++ normalUsage/src/routes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+
+const routes = createBrowserRouter([
+    {
+        path: "/",
+        Component: "",
+    },
+]);
+
+export default routes;
