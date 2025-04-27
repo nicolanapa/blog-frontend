9704b293e3fc8ee14e12fdd2955078f0253ffe31
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-27 22:02:57 +0200

    feat: First version of routes

--- normalUsage/src/routes.jsx
+++ normalUsage/src/routes.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
 
+// Import all Classes
+
+// Use loaders ?
+
+// Fix syntax issue in Component
+
 const routes = createBrowserRouter([
     {
         path: "/",
-        Component: "",
+        Component: <Home />,
+    },
+    {
+        path: "/user/:id",
+        Component: <User />,
+    },
+    {
+        path: "/post/:id",
+        Component: <Post />,
+    },
+    {
+        path: "/authenticate",
+        Component: <AuthenticatePage />,
     },
 ]);
 
