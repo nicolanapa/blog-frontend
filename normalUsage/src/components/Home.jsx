db6174d6d673e36807e5054b6a315a11b861d247
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-28 23:12:35 +0200

    feat: Created basic class to fix the routes in routes.jsx

---
+++ normalUsage/src/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+    return <>Test</>;
+}
+
+export default Home;
---
+++ normalUsage/src/components/Post.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Post() {
+    return <>Test3</>;
+}
+
+export default Post;
