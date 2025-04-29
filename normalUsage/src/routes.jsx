558d4f64303c602af6e21e1c30362ca138f1ada8
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-29 23:05:04 +0200

    feat: Layout Router using Layout

--- normalUsage/src/routes.jsx
+++ normalUsage/src/routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router";
 
+import Layout from "./components/Layout.jsx";
 import Home from "./components/Home";
 import User from "./components/User";
 import Post from "./components/Post";
@@ -9,21 +10,26 @@ import allPosts from "./loaders/allPosts.js";
 
 const routes = createBrowserRouter([
     {
-        path: "/",
-        loader: allPosts,
-        Component: Home,
-    },
-    {
-        path: "/user/:id",
-        Component: User,
-    },
-    {
-        path: "/post/:id",
-        Component: Post,
-    },
-    {
-        path: "/authenticate",
-        Component: AuthenticatePage,
+        Component: Layout,
+        children: [
+            {
+                path: "/",
+                loader: allPosts,
+                Component: Home,
+            },
+            {
+                path: "/user/:id",
+                Component: User,
+            },
+            {
+                path: "/post/:id",
+                Component: Post,
+            },
+            {
+                path: "/authenticate",
+                Component: AuthenticatePage,
+            },
+        ],
     },
 ]);
 
