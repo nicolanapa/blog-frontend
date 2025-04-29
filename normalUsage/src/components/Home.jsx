923a3e49cc852e41991aab40e1ce003fcd4bcb57
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-29 23:22:09 +0200

    feat: Mapping of posts to return PostPreview

--- normalUsage/src/components/Home.jsx
+++ normalUsage/src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { useLoaderData } from "react-router";
+import PostPreview from "./PostPreview";
 
 function Home() {
     const { posts } = useLoaderData();
 
-    return <>Test</>;
+    return (
+        <main>
+            {posts.map((post) => (
+                <PostPreview props={post} key={post.id} />
+            ))}
+        </main>
+    );
 }
 
 export default Home;
