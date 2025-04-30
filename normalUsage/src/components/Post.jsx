b748a427fd7761dc71a24c91f776bfc66f60ad13
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-01 00:04:11 +0200

    feat: Post view with a section of comments
    Added semantic tags used in PostPreview

--- normalUsage/src/components/Post.jsx
+++ normalUsage/src/components/Post.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Link, useLoaderData } from "react-router";
+import Comment from "./Comment";
 
 function Post() {
-    return <>Test3</>;
+    const { post, comments } = useLoaderData();
+
+    console.log(post, comments);
+
+    return (
+        <article>
+            <header>
+                <small>
+                    <time dateTime={post.publishDate}>
+                        {new Date(post.publishDate).toLocaleString()}
+                    </time>
+                </small>
+
+                <address>
+                    Posted by <Link to={"/user/" + post.userId}>User</Link>
+                </address>
+
+                <h2>
+                    <Link to={"/post/" + post.id}>{post.title}</Link>
+                </h2>
+            </header>
+
+            <p>{post.content}</p>
+
+            <section>
+                <h3>Comments</h3>
+
+                {comments.map((comment) => (
+                    <Comment props={comment} key={comment.id} />
+                ))}
+            </section>
+        </article>
+    );
 }
 
 export default Post;
