0b25608066643a8d3758b36e30fd622cb7cd5e1c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-30 23:55:41 +0200

    feat: Added semantic time and address to link to User page

--- normalUsage/src/components/PostPreview.jsx
+++ normalUsage/src/components/PostPreview.jsx
@@ -4,11 +4,19 @@ import { Link } from "react-router";
 function PostPreview({ props }) {
     return (
         <article>
-            <small>{props.publishDate}</small>
+            <small>
+                <time dateTime={props.publishDate}>
+                    {new Date(props.publishDate).toLocaleString()}
+                </time>
+            </small>
+
+            <address>
+                Posted by <Link to={"/user/" + props.userId}>User</Link>
+            </address>
+
             <h3>
                 <Link to={"/post/" + props.id}>{props.title}</Link>
             </h3>
-            <p>{String(props.content).substring(0, 30)}...</p>
         </article>
     );
 }
