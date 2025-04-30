a1c327e8254d23bc26a9bb098378c6f9b0bbe293
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-04-30 23:21:59 +0200

    feat: Error Boundary Component and user/post loaders

--- normalUsage/src/routes.jsx
+++ normalUsage/src/routes.jsx
@@ -1,16 +1,21 @@
+import React from "react";
 import { createBrowserRouter } from "react-router";
 
 import Layout from "./components/Layout.jsx";
+import Error from "./components/Error.jsx";
 import Home from "./components/Home";
 import User from "./components/User";
 import Post from "./components/Post";
 import AuthenticatePage from "./components/AuthenticatePage";
 
 import allPosts from "./loaders/allPosts.js";
+import user from "./loaders/User.js";
+import post from "./loaders/Post.js";
 
 const routes = createBrowserRouter([
     {
         Component: Layout,
+        errorElement: <Error />,
         children: [
             {
                 path: "/",
@@ -19,10 +24,12 @@ const routes = createBrowserRouter([
             },
             {
                 path: "/user/:id",
+                loader: user,
                 Component: User,
             },
             {
                 path: "/post/:id",
+                loader: post,
                 Component: Post,
             },
             {
