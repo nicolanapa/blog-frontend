a941b54dc3e4a031a9cd336a63eac1aadcf67227
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-02 18:57:44 +0200

    feat: iframe tags for login and signup forms

--- normalUsage/src/components/AuthenticatePage.jsx
+++ normalUsage/src/components/AuthenticatePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useLoaderData } from "react-router";
+import Login from "./Login";
+import Signup from "./Signup";
 
 function AuthenticatePage() {
-    return <>Test4</>;
+    const { isCurrentlyLoggedIn } = useLoaderData();
+
+    console.log(isCurrentlyLoggedIn);
+
+    return (
+        <main>
+            <iframe title="Login" src="../login/frame" />
+
+            <iframe title="Signup" src="../signup/frame" />
+        </main>
+    );
 }
 
 export default AuthenticatePage;
