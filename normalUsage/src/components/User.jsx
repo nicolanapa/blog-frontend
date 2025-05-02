5b07f72c5bed4524d7e80b00d7823d07ab4d6eae
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-02 13:59:42 +0200

    feat: User Info and posts if blogAuthor

--- normalUsage/src/components/User.jsx
+++ normalUsage/src/components/User.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useLoaderData } from "react-router";
+import PostPreview from "./PostPreview";
 
 function User() {
-    return <>Test2</>;
+    const { user, posts } = useLoaderData();
+
+    return (
+        <main>
+            <div>
+                <p>{user.type === "normalUser" ? "User" : "Blog Author"}</p>
+                <address>{user.username}</address>
+                <small>{user.id}</small>
+            </div>
+
+            {user.type === "blogAuthor" ? (
+                <div>
+                    <h2>Published Posts</h2>
+
+                    {posts.map((post) => (
+                        <PostPreview props={post} />
+                    ))}
+                </div>
+            ) : (
+                ""
+            )}
+        </main>
+    );
 }
 
 export default User;
