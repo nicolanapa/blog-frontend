014783cc2d0f191e350a24d323f397cd724048e0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-05 22:02:31 +0200

    refactor: Moved all the correct code outside an if

--- normalUsage/src/actions/addComment.js
+++ normalUsage/src/actions/addComment.js
@@ -7,48 +7,48 @@ async function addComment({ request }) {
 
     const data = await request.formData();
 
-    if (data.get("content").length >= 1 && data.get("content").length <= 1024) {
-        let postId = request.url.split("/");
-        postId = postId[postId.length - 2];
-
-        const commentRequest = await fetch(
-            import.meta.env.VITE_FULL_HOSTNAME + "/comment/" + postId,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + JwtTokenHandler.getToken(),
-                },
-                body: JSON.stringify({
-                    content: data.get("content"),
-                }),
-            }
-        );
-
-        if (commentRequest.status === 401) {
-            return {
-                ok: commentRequest.ok,
-                error: "Unauthorized, please login through /authenticate",
-            };
-        }
-
-        const commentResponse = await commentRequest.json();
-
-        console.log(commentRequest, commentResponse);
-
-        if (!commentRequest.ok) {
-            console.log(commentResponse);
-
-            return { ok: commentRequest.ok, error: commentResponse.errors };
-        }
-
-        return { ok: commentRequest.ok };
+    if (data.get("content").length === 0 || data.get("content").length > 1024) {
+        return {
+            ok: false,
+            error: "Invalid Comment, this is field is required or reached max 1024 characters",
+        };
     }
 
-    return {
-        ok: false,
-        error: "Invalid Comment, this is field is required or reached max 1024 characters",
-    };
+    let postId = request.url.split("/");
+    postId = postId[postId.length - 2];
+
+    const commentRequest = await fetch(
+        import.meta.env.VITE_FULL_HOSTNAME + "/comment/" + postId,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + JwtTokenHandler.getToken(),
+            },
+            body: JSON.stringify({
+                content: data.get("content"),
+            }),
+        }
+    );
+
+    if (commentRequest.status === 401) {
+        return {
+            ok: commentRequest.ok,
+            error: "Unauthorized, please login through /authenticate",
+        };
+    }
+
+    const commentResponse = await commentRequest.json();
+
+    console.log(commentRequest, commentResponse);
+
+    if (!commentRequest.ok) {
+        console.log(commentResponse);
+
+        return { ok: commentRequest.ok, error: commentResponse.errors };
+    }
+
+    return { ok: commentRequest.ok };
 }
 
 export default addComment;
