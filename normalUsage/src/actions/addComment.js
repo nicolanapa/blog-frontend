b8ea9aa4a4569018dac1ac46ee6fd3e3561ee956
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-05 22:00:46 +0200

    feat: Action that fetches POST /comment/:postId and adds a new Comment

---
+++ normalUsage/src/actions/addComment.js
@@ -0,0 +1,54 @@
+import JwtTokenHandler from "../JwtTokenHandler";
+
+async function addComment({ request }) {
+    if (!JwtTokenHandler.isCurrentlyLoggedIn()) {
+        return { ok: false, error: "You're not currently logged in" };
+    }
+
+    const data = await request.formData();
+
+    if (data.get("content").length >= 1 && data.get("content").length <= 1024) {
+        let postId = request.url.split("/");
+        postId = postId[postId.length - 2];
+
+        const commentRequest = await fetch(
+            import.meta.env.VITE_FULL_HOSTNAME + "/comment/" + postId,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + JwtTokenHandler.getToken(),
+                },
+                body: JSON.stringify({
+                    content: data.get("content"),
+                }),
+            }
+        );
+
+        if (commentRequest.status === 401) {
+            return {
+                ok: commentRequest.ok,
+                error: "Unauthorized, please login through /authenticate",
+            };
+        }
+
+        const commentResponse = await commentRequest.json();
+
+        console.log(commentRequest, commentResponse);
+
+        if (!commentRequest.ok) {
+            console.log(commentResponse);
+
+            return { ok: commentRequest.ok, error: commentResponse.errors };
+        }
+
+        return { ok: commentRequest.ok };
+    }
+
+    return {
+        ok: false,
+        error: "Invalid Comment, this is field is required or reached max 1024 characters",
+    };
+}
+
+export default addComment;
