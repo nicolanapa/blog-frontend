efb6f456258d8cdfac38c96914a1bd90e196a79e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-05 21:59:57 +0200

    feat: fetcher Form

--- normalUsage/src/components/AddComment.jsx
+++ normalUsage/src/components/AddComment.jsx
@@ -1,5 +1,26 @@
+import React from "react";
+import { useFetcher } from "react-router";
+
 function AddComment({ postId }) {
-    console.log(postId);
+    const fetcher = useFetcher();
+
+    return (
+        <fetcher.Form method="post" action="./addComment">
+            {fetcher.data?.ok && <p>Success</p>}
+            {fetcher.data?.error && <p>{fetcher.data.error}</p>}
+
+            <label htmlFor="content">Leave a Comment!</label>
+            <input
+                type="text"
+                id="content"
+                name="content"
+                maxLength={1024}
+                required
+            />
+
+            <button type="submit">Submit</button>
+        </fetcher.Form>
+    );
 }
 
 export default AddComment;
