f3f6aa0bb05f69e536b6a84d6b718b284359f95d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-07 20:50:44 +0200

    feat: Working header and State for userId

--- normalUsage/src/components/Layout.jsx
+++ normalUsage/src/components/Layout.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, Outlet } from "react-router";
+import LoggedInContext from "../context/LoggedInContext";
+import { jwtDecode } from "jwt-decode";
+import JwtTokenHandler from "../scripts/JwtTokenHandler";
+import "../styles/layout.css";
 
 function Layout() {
+    const { isLoggedIn } = useContext(LoggedInContext);
+    const [userId, setUserId] = useState(0);
+
+    useEffect(() => {
+        if (JwtTokenHandler.isCurrentlyLoggedIn()) {
+            const decodedJwt = jwtDecode(JwtTokenHandler.getToken());
+            setUserId(decodedJwt.id);
+        }
+    }, [isLoggedIn]);
+
     return (
         <>
             <header>
-                Header
-                <nav>Home, Users, your Profile...</nav>
+                <nav>
+                    <h2>
+                        <Link to={"/"}>Home</Link>
+                    </h2>
+
+                    <h2>
+                        <Link
+                            to={
+                                isLoggedIn ? "/user/" + userId : "/authenticate"
+                            }
+                        >
+                            {isLoggedIn ? "Your Profile" : "Login / Signup"}
+                        </Link>
+                    </h2>
+                </nav>
             </header>
 
             <Outlet />
