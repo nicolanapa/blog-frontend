25561b56c69cad5384a9031939554ccef0873d70
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-11 19:59:39 +0200

    feat: Divided posts in published and not published with the use of the jwt token

--- blogAdmin/src/loaders/allPosts.js
+++ blogAdmin/src/loaders/allPosts.js
@@ -1,7 +1,22 @@
+import JwtTokenHandler from "../scripts/JwtTokenHandler";
+
 async function allPosts() {
     return {
-        posts: await fetch(
+        publishedPosts: await fetch(
             import.meta.env.VITE_FULL_HOSTNAME + "/post/published"
+        )
+            .then((res) => res.json())
+            .catch((e) => {
+                console.error(e);
+                return [];
+            }),
+        unpublishedPosts: await fetch(
+            import.meta.env.VITE_FULL_HOSTNAME + "/post/unpublished",
+            {
+                headers: {
+                    Authorization: "Bearer " + JwtTokenHandler.getToken(),
+                },
+            }
         )
             .then((res) => res.json())
             .catch((e) => {
