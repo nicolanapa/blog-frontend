66142cc7f700a899bcc685da45fca4f65ab3069a
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-11 20:56:35 +0200

    feat: Divided the posts into published and unpublished
    Added a new Button linking to /newPost

--- blogAdmin/src/components/Home.jsx
+++ blogAdmin/src/components/Home.jsx
@@ -1,28 +1,70 @@
-import React from "react";
-import { useLoaderData } from "react-router";
+import React, { useState } from "react";
+import { Link, useLoaderData } from "react-router";
 import PostPreview from "./PostPreview";
 import "../styles/post.css";
 
 function Home() {
-    const { posts } = useLoaderData();
+    const { publishedPosts, unpublishedPosts } = useLoaderData();
+    const [showPublishedPosts, setShowPublishedPosts] = useState(true);
 
     return (
-        <main className="post-preview-container">
-            {posts.map((post, i) => {
-                if (i > 20) {
-                    return;
-                }
+        <main>
+            <div>
+                <button
+                    onClick={() => {
+                        setShowPublishedPosts(true);
+                    }}
+                >
+                    Published
+                </button>
+                <button
+                    onClick={() => {
+                        setShowPublishedPosts(false);
+                    }}
+                >
+                    Unpublished
+                </button>
 
-                return (
-                    <PostPreview
-                        id={post.id}
-                        userId={post.userId}
-                        title={post.title}
-                        publishDate={post.publishDate}
-                        key={post.id}
-                    />
-                );
-            })}
+                <Link to="/newPost">
+                    <button>New Post</button>
+                </Link>
+            </div>
+
+            <div className="post-preview-container">
+                {showPublishedPosts &&
+                    publishedPosts.map((post, i) => {
+                        if (i > 20) {
+                            return;
+                        }
+
+                        return (
+                            <PostPreview
+                                id={post.id}
+                                userId={post.userId}
+                                title={post.title}
+                                publishDate={post.publishDate}
+                                key={post.id}
+                            />
+                        );
+                    })}
+
+                {!showPublishedPosts &&
+                    unpublishedPosts.map((post, i) => {
+                        if (i > 20) {
+                            return;
+                        }
+
+                        return (
+                            <PostPreview
+                                id={post.id}
+                                userId={post.userId}
+                                title={post.title}
+                                publishDate={post.publishDate}
+                                key={post.id}
+                            />
+                        );
+                    })}
+            </div>
         </main>
     );
 }
