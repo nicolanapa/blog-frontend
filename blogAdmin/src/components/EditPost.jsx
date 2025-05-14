cccd0a0f50503f3c7e200cecd225e6011b10a051
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-14 19:06:16 +0200

    feat!: Edit view in Post

---
+++ blogAdmin/src/components/EditPost.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { useFetcher } from "react-router";
+
+function EditPost(props) {
+    const fetcher = useFetcher();
+
+    return (
+        <fetcher.Form method="post" action={"/post/" + props.id + "/edit"}>
+            {fetcher.data?.ok && <p className="success">Success!</p>}
+            {fetcher.data?.error && (
+                <p className="error">{fetcher.data.error}</p>
+            )}
+
+            <label htmlFor="title">Title: </label>
+            <input
+                type="text"
+                id="title"
+                name="title"
+                defaultValue={props.title}
+                maxLength={32}
+                required
+            />
+
+            <label htmlFor="content">Content: </label>
+            <textarea
+                type="text"
+                id="content"
+                name="content"
+                defaultValue={props.content}
+                maxLength={4096}
+                required
+            />
+
+            <label htmlFor="is-published">Post is set as </label>
+            <select
+                id="is-published"
+                name="isPublished"
+                defaultValue={props.isPublished}
+            >
+                <option value="true" defaultValue={true}>
+                    Published
+                </option>
+                <option value="false">Unpublished</option>
+            </select>
+
+            <button type="submit">Post</button>
+        </fetcher.Form>
+    );
+}
+
+EditPost.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    isPublished: PropTypes.bool.isRequired,
+};
+
+export default EditPost;
--- blogAdmin/src/components/Post.jsx
+++ blogAdmin/src/components/Post.jsx
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLoaderData } from "react-router";
 import Comment from "./Comment";
 import AddComment from "./AddComment";
 import "../styles/post.css";
+import EditPost from "./EditPost";
 
 function Post() {
     const { post, comments } = useLoaderData();
+    const [viewMode, setViewMode] = useState(true);
 
     return (
         <article className="post-container">
-            <header className="post-info">
-                <small className="date">
-                    <time dateTime={post.publishDate}>
-                        {new Date(post.publishDate).toLocaleString()}
-                    </time>
-                </small>
+            {viewMode && (
+                <>
+                    <header className="post-info">
+                        <small className="date">
+                            <time dateTime={post.publishDate}>
+                                {new Date(post.publishDate).toLocaleString()}
+                            </time>
+                        </small>
 
-                <address>
-                    Posted by <Link to={"/user/" + post.userId}>User</Link>
-                </address>
+                        <address>
+                            Posted by{" "}
+                            <Link to={"/user/" + post.userId}>User</Link>
+                        </address>
 
-                <h2>
-                    <Link to={"/post/" + post.id}>{post.title}</Link>
-                </h2>
-            </header>
+                        <h2>
+                            <Link to={"/post/" + post.id}>{post.title}</Link>
+                        </h2>
+                    </header>
 
-            <p>{post.content}</p>
+                    <p>{post.content}</p>
+                </>
+            )}
+
+            {!viewMode && (
+                <EditPost
+                    id={post.id}
+                    title={post.title}
+                    content={post.content}
+                    isPublished={post.isPublished}
+                />
+            )}
+
+            <button type="button" onClick={() => setViewMode(!viewMode)}>
+                <img
+                    src="/icons/edit.svg"
+                    alt="Edit this post and go into edit mode"
+                    width="30px"
+                    height="auto"
+                />
+            </button>
 
             <section className="comments-container">
                 <h3>Comments</h3>
