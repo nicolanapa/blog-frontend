042953ecce86b7100309a1b1b21b869c325bc3ea
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-14 18:37:27 +0200

    feat: Button that changes the isPublished property of a Post

--- blogAdmin/src/components/PostPreview.jsx
+++ blogAdmin/src/components/PostPreview.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 import PropTypes from "prop-types";
+import changePublishState from "../scripts/changePublishState";
 import "../styles/post.css";
 
 function PostPreview(props) {
+    const [isPublished, setIsPublished] = useState(props.isPublished);
+
     return (
         <article className="post-preview">
-            <img src="/icons/post.svg" alt="Post" width="48px" height="auto" className="preview-photo" />
+            <img
+                src="/icons/post.svg"
+                alt="Post"
+                width="48px"
+                height="auto"
+                className="preview-photo"
+            />
 
             <small>
                 <time dateTime={props.publishDate}>
@@ -21,6 +30,15 @@ function PostPreview(props) {
             <h3>
                 <Link to={"/post/" + props.id}>{props.title}</Link>
             </h3>
+
+            <button
+                type="button"
+                onClick={() =>
+                    changePublishState(props, setIsPublished, isPublished)
+                }
+            >
+                {isPublished ? "Unpublish" : "Publish"}
+            </button>
         </article>
     );
 }
@@ -29,7 +47,9 @@ PostPreview.propTypes = {
     id: PropTypes.number.isRequired,
     userId: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
     publishDate: PropTypes.instanceOf(Date).isRequired,
+    isPublished: PropTypes.bool.isRequired,
 };
 
 export default PostPreview;
