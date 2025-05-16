62c5be9eef194dd5ec2b95fbcee4272624250374
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-05-16 19:15:40 +0200

    fix!: Moved vite.config.js to each subdirectory and changed way of importing env variables

--- blogAdmin/vite.config.js
+++ blogAdmin/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        allowedHosts: [import.meta.env.VITE_HOSTNAME],
-        port: import.meta.env.VITE_PORT,
+        allowedHosts: [process.env.VITE_HOSTNAME],
+        port: process.env.VITE_PORT,
     },
 });
